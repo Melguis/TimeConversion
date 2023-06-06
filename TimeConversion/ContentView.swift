9a8e2b0b818b4d3dbbb5accfc808bdd5b4f2c864
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-06 10:49:10 -0300

    Initial Commit

---
+++ TimeConversion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TimeConversion
+//
+//  Created by Jorge Henrique on 06/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Jorge Henrique on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
